e5f0562040c8fcf3ab1d988cdb27f7c7fae5b97a
Miguel_Martinez <mm6980183@gmail.com>
2025-12-02 16:11:25 -0500

    Actualización de Login

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import LoginPage from './pages/auth/login'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <LoginPage></LoginPage>
+  )
+}
+
+export default App
---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,41 @@
+import './auth.css'
+
+const LoginPage = () => {
+    return (
+        <div className="container">
+            <div className="right">
+                <div className="login-box">
+                    <h1>Pay<span style={{ color: "#3b82f6" }}>Track</span></h1>
+                    <p>Bienvenido de vuelta</p>
+
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="email">Correo electrónico</label>
+                            <input type="email" id="email" placeholder="Ingresa tu email" required />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="password">Contraseña</label>
+                            <input type="password" id="password" placeholder="Ingresa tu contraseña" required />
+                        </div>
+
+                        <button className="btn" type="submit">Iniciar sesión</button>
+                    </form>
+
+                    <div className="social-login">
+                        <p>O inicia sesión con:</p>
+                        <a href="#" className="social-btn">Facebook</a>
+                        <a href="#" className="social-btn">Google</a>
+                    </div>
+
+                    <div className="links">
+                        <p>¿Olvidaste tu contraseña? <a href="#">Recupérala aquí</a></p>
+                        <p>¿No tienes una cuenta? <a href="#">Crea una ahora</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+export default LoginPage 
